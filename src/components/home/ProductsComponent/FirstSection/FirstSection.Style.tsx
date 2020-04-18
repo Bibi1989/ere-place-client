811ee3c9d812536f31fad2dca00bd9fcded1947e
bibi1989 <bibiaremieye@gmail.com>
2020-04-18 08:45:24 +0100

    redid the navbar and made it responsive

--- src/components/home/ProductsComponent/FirstSection/FirstSection.Style.tsx
+++ src/components/home/ProductsComponent/FirstSection/FirstSection.Style.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Image = styled.div`
   .first-section-image {
-    height: 70vh;
+    max-height: 70vh;
     img {
       width: 100%;
       height: 100%;
